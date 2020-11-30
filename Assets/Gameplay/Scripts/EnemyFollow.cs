111eff43b6088fffa7bdb6e551b35fc2f4815433
Caupo Helvik <helvikcaupo@gmail.com>
2020-11-30 22:54:14 +0200

    Enemy flips sprites where it moves and stopped floating after collision

--- Assets/Gameplay/Scripts/EnemyFollow.cs
+++ Assets/Gameplay/Scripts/EnemyFollow.cs
@@ -7,6 +7,8 @@ public class EnemyFollow : MonoBehaviour
     public float speed;
     public Transform target;
     public float stoppingDist = 3;
+    bool isFacingRight = false;
+    bool prevIsFacingRight = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,29 @@ public class EnemyFollow : MonoBehaviour
         if(Vector2.Distance(transform.position, target.position) > stoppingDist)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            if(transform.position.x > target.position.x)
+            {
+                isFacingRight = false;
+            }
+            else
+            {
+                isFacingRight = true;
+            }
+
+            if(prevIsFacingRight != isFacingRight)
+            {
+                Flip();
+            }
+
+            prevIsFacingRight = isFacingRight;
         }
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
