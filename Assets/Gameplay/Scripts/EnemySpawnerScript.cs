2410e6e63fd26b627fe6519a9cde4b9db76263ae
Caupo Helvik <helvikcaupo@gmail.com>
2020-12-03 21:26:33 +0200

    Made enemy spawner script so that all spawners need to have enemies to be 0 to work

--- Assets/Gameplay/Scripts/EnemySpawnerScript.cs
+++ Assets/Gameplay/Scripts/EnemySpawnerScript.cs
@@ -21,28 +21,75 @@ public class EnemySpawnerScript : MonoBehaviour
         selfX = gameObject.transform.position.x;
     }
 
+    void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Destroy(gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Time.time > nextSpawn)
         {
-            if(!infiniteAmount)
-            {
-                enemyCount--;
+            nextSpawn = Time.time + spawnRate;
+            OnNextSpawnTime();
+        }
+    }
 
-                if(enemyCount <= 0)
+    void OnNextSpawnTime()
+    {
+        if (!infiniteAmount)
+        {
+            enemyCount--;
+
+            if (enemyCount <= 0)
+            {
+                if(HaveAllEnemiesBeenKilled())
                 {
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                    Destroy(gameObject);
+                    GoToNextScene();
                 }
             }
-
-            nextSpawn = Time.time + spawnRate;
-            randomX = Random.Range(-1f, 1f);
-            spawnPos = new Vector2(randomX + selfX, transform.position.y);
-
-            Instantiate(PickRandomEnemy(), spawnPos, Quaternion.identity);
         }
+
+        if (enemyCount > 0 || infiniteAmount)
+        {
+            SpawnRandomEnemy();
+        }
+    }
+
+    bool HaveAllEnemiesBeenKilled()
+    {
+        GameObject[] gameObjects = FindObjectsOfType<GameObject>();
+
+        foreach(GameObject gameObject in gameObjects)
+        {
+            if(gameObject.tag == "EnemySpawner")
+            {
+                EnemySpawnerScript scriptObj = gameObject.GetComponent<EnemySpawnerScript>();
+
+                if(scriptObj != null && scriptObj.enemyCount > 0)
+                {
+                    Debug.Log("ScriptObj: " + scriptObj.enemyCount);
+                    return false;
+                }
+            }
+            else if (gameObject.tag == "Enemy")
+            {
+                Debug.Log("There is obj with Enemy tag");
+                return false;
+            }
+        }
+
+        Debug.Log("ALL ENEMIES KILLED");
+        return true;
+    }
+
+    void SpawnRandomEnemy()
+    {
+        randomX = Random.Range(-1f, 1f);
+        spawnPos = new Vector2(randomX + selfX, transform.position.y);
+        Instantiate(PickRandomEnemy(), spawnPos, Quaternion.identity);
     }
 
     GameObject PickRandomEnemy()
