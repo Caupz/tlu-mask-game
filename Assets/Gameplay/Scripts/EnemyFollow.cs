2f962cbb3c5068bb14f9a57fad5a1e153a39b3f9
Caupo Helvik <helvikcaupo@gmail.com>
2020-12-05 18:37:15 +0200

    Added jumping ability to the enemies

--- Assets/Gameplay/Scripts/EnemyFollow.cs
+++ Assets/Gameplay/Scripts/EnemyFollow.cs
@@ -9,21 +9,95 @@ public class EnemyFollow : MonoBehaviour
     public float stoppingDist = 3;
     public bool isFacingRight = false;
     bool prevIsFacingRight = false;
+    public bool canJump = false;
+    public float jumpRate;
+
+    float jumpCooldown = 0;
+    float prevSec = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        jumpCooldown = Time.time + jumpRate;
+    }
+
+    void OnSecondUpdate()
+    {
+        jumpCooldown--;
+
+        if (jumpCooldown <= 0)
+        {
+            jumpCooldown = Time.time + jumpRate;
+            Jump();
+        }
+    }
+
+    bool jumping = false;
+    float jumpingDuration = 1f;
+
+    void Jump()
+    {
+        jumping = true;
+        jumpingDuration = 0.5f;
+    }
+
+    void ProcessJumping(Vector3 pos)
+    {
+        if(jumpingDuration > 0)
+        {
+            jumpingDuration -= Time.deltaTime;
+        }
+
+        if (jumpingDuration < 0.15f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, (speed * 4.5f) * Time.deltaTime);
+        }
+        else if (jumpingDuration < 0.25f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, (speed * 2.5f) * Time.deltaTime);
+        }
+        else if (jumpingDuration < 0.35f)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
+            targetPos.y += 2.5f;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, (speed * 6.0f) * Time.deltaTime);
+        }
+
+        if (jumpingDuration <= 0)
+        {
+            jumping = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) > stoppingDist)
+        if (Time.time > prevSec)
+        {
+            prevSec = Time.time + 1;
+            OnSecondUpdate();
+        }
+
+        if(jumping)
+        {
+            ProcessJumping(transform.position);
+        }
+        else
+        {
+            Walk();
+        }
+
+        
+    }
+
+    void Walk()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDist)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-            if(transform.position.x > target.position.x)
+            if (transform.position.x > target.position.x)
             {
                 isFacingRight = false;
             }
@@ -32,7 +106,7 @@ public class EnemyFollow : MonoBehaviour
                 isFacingRight = true;
             }
 
-            if(prevIsFacingRight != isFacingRight)
+            if (prevIsFacingRight != isFacingRight)
             {
                 Flip();
             }
